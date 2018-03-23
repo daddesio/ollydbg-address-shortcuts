14e8e7825d34441f2faf364a2e700cd5b2c78b78
Andrew D'Addesio <modchipv12@gmail.com>
2018-03-23 14:01:10 -0500

    Update 2018
    * Get rid of "Copy dword relative to ..." shortcuts (I rarely used
    them).
    * Simpler names ("RVA", "File Offset").
    * Distinguish between pretty/non-pretty addresses.
    * Add Readme.md and screenshot.png.

---
+++ Readme.md
@@ -0,0 +1,40 @@
+## Overview
+
+This plugin adds the following menu options / shortcuts to OllyDbg v2.01:
+
+* **Follow DWORD in Disassember** (Ctrl+Enter)
+* **Follow DWORD in Dump** (Alt+Enter)
+* **Follow DWORD in Stack**
+* **Copy RVA** &mdash; Similar to Alt+Ins in Olly, except that the address will be relative to the start of the module.
+* **Copy RVA (pretty)** (Alt+"+") &mdash; The string will be formatted as `helloworld+0x36a4`.
+* **Copy file offset** &mdash; Similar to Alt+Ins in Olly, except that the address will be relative to the start of the file.
+* **Copy file offset (pretty)** (Ctrl+"+") &mdash; The string will be formatted as `helloworld.dll+0x36a4`. **Note:** On most exe's (GCC, MSVC, etc.), the RVA and file offset are the same. This command matters mainly for hand-crafted executables such as demoscene.
+
+**Note:** The "+" key is at the top row of the keyboard, *not* the number pad.
+
+The above keyboard shortcuts (Alt vs. Ctrl, etc.) were chosen rather arbitrarily, so feel free to change them.
+
+## Usage
+
+Simply drop address_shortcuts.dll next to your OllyDbg.exe, then restart OllyDbg, and Address Shortcuts should appear under your right-click menu.
+
+Now, look at a C++ object (with virtual functions) in the Memory Dump pane. The first dword should be a vtable address. Press Alt+Enter to follow it. Now press Ctrl+Enter to quickly pull up any function in the Disassembly Pane, and Alt+"+" to copy its RVA in a pretty format (helloworld+0x36a4). And, as usual, use the familiar Numpad-Plus/Minus to navigate forwards/backwards and Alt+Ins to copy raw addresses. Copy/paste hex bytes with Ctrl+Ins/Shift+Ins, and generate pretty hexdumps with Ctrl+C.
+
+## Compiling
+
+You will need the [OllyDbg Plugin SDK (ollydbg.lib)](http://ollydbg.de/plug201h.zip).
+
+Using GCC:
+
+Copy CodeBlocks/plugin.h and CodeBlocks/ollydbg.lib from plug201h.zip into the current directory, then run:
+
+    gcc -std=c99 -Wall -m32 -Os -g0 -funsigned-char -shared -nostartfiles -s -static-libgcc -o address_shortcuts.dll address_shortcuts.c ollydbg.lib
+
+Using MSVC:
+
+TODO...
+
+## To do
+
+* I'd prefer having Enter instead of Alt+Enter to follow in dump. This nearly works, except that OllyDbg also pops open an "Edit data at address" dialog in response to the enter key. We could patch Ollydbg to prevent this dialog (in response to enter only), but it would complicate this otherwise very simple script.
+* Get pretty RVA's in other debuggers as well (x64dbg, windbg, gdb, ...).
--- address_shortcuts.c
+++ address_shortcuts.c
@@ -1,26 +1,9 @@
 /*
-** OllyDbg v2.0 Address Shortcuts plugin - address_shortcuts.c
-** Authors: Andrew D'Addesio <andrew@fatbag.net>
+** OllyDbg Address Shortcuts plugin (for OllyDbg v2.01) - address_shortcuts.c
+** Authors: Andrew D'Addesio
 ** License: Public domain
-** Compile: gcc -std=c99 -Wall -Wextra -Wno-unused-parameter -m32 -Os -g0 -fomit-frame-pointer
-**	-funsigned-char -shared -nostartfiles -s -static-libgcc -o address_shortcuts.dll
-**	address_shortcuts.c ollydbg.lib
-**
-** This plugin adds the following 6 shortcuts to OllyDbg:
-** (*) Follow DWORD in Dump (Alt+Enter)
-** (*) Follow DWORD in Disassembler (Ctrl+Enter)
-** (*) Copy address base relative (Alt+"+")
-** (*) Copy address file relative (Ctrl+"+")
-** (*) Copy DWORD base relative (Alt+"-")
-** (*) Copy DWORD file relative (Ctrl+"-").
-** where the "+" and "-" keys are at the top row of the keyboard, *not* the number pad.
-**
-** Base or file relative addresses will be copied to the clipboard in the form:
-**     mymodule_base+0x26c0 (base-relative)
-** or
-**     mymodule_file+0x26c0 (file-relative).
-**
-** Personally, I find the first three shortcuts to be essential.
+** Compile: gcc -std=c99 -Wall -m32 -Os -g0 -funsigned-char -shared -nostartfiles -s
+**          -static-libgcc -o address_shortcuts.dll address_shortcuts.c ollydbg.lib
 */
 
 #define _CRT_SECURE_NO_DEPRECATE
@@ -51,17 +34,39 @@ static uint32_t get_selection(t_table *pt)
 	return 0;
 }
 
-enum rel_mode {
-	BASE_RELATIVE,
-	FILE_RELATIVE
+static const wchar_t *get_filename(const wchar_t *path)
+{
+	const wchar_t *last_slash = path-1;
+
+	while (*path != 0) {
+		if (*path == '/' || *path == '\\')
+			last_slash = path;
+		path++;
+	}
+
+	return last_slash+1;
+}
+
+enum copy_mode {
+	rva,
+	rva_pretty,
+	file_offset,
+	file_offset_pretty
 };
 
-static void copy_to_clipboard(uint32_t address, enum rel_mode mode)
+enum follow_mode {
+	asm,
+	dump,
+	stack
+};
+
+static void copy_to_clipboard(uint32_t address, enum copy_mode mode)
 {
 	t_module *m;
 	HANDLE hData;
 	wchar_t *str;
 	uint32_t offset;
+	const wchar_t *name;
 
 	m = Findmodule(address);
 	if (!m) {
@@ -69,77 +74,72 @@ static void copy_to_clipboard(uint32_t address, enum rel_mode mode)
 		return;
 	}
 
-	switch (mode) {
-	case BASE_RELATIVE:
+	if (mode == rva || mode == rva_pretty) {
 		offset = address - m->base;
-		break;
-	case FILE_RELATIVE: default:
-		if (mode == FILE_RELATIVE) {
-			if (address == m->base)
-				offset = 0;
-			else {
-				offset = Findfileoffset(m, address);
-				if (offset == 0) {
-					Flash(L"Address has no physical mapping in file");
-					return;
-				}
-			}
+		name = m->modname;
+	} else {
+		offset = Findfileoffset(m, address);
+		if (offset == 0) {
+			Flash(L"Address has no physical mapping in file");
+			return;
 		}
+
+		name = get_filename(m->path);
 	}
 
 	/* Copy to the clipboard. */
 
-	hData = GlobalAlloc(GMEM_MOVEABLE, 1024*sizeof(wchar_t));
+	hData = GlobalAlloc(GMEM_MOVEABLE, (wcslen(name)+11+1)*sizeof(wchar_t));
 	if (!hData)
 		return;
 
 	str = GlobalLock(hData);
-	if (!str) {
-		GlobalFree(hData);
-		return;
-	}
-	wsprintf(str, L"%s%s+0x%x", m->modname, mode == BASE_RELATIVE ? L"_base" : L"_file", offset);
+	if (!str)
+		goto close_hdata;
+
+	if (mode == rva_pretty || mode == file_offset_pretty)
+		wsprintf(str, L"%s+0x%x", name, offset);
+	else
+		wsprintf(str, L"%08X", offset);
+
 	GlobalUnlock(hData);
 
-	if (!OpenClipboard(NULL)) {
-		GlobalFree(hData);
-		return;
-	}
+	if (!OpenClipboard(NULL))
+		goto close_hdata;
 
-	EmptyClipboard();
-	SetClipboardData(CF_UNICODETEXT, hData);
+	if (!EmptyClipboard())
+		goto close_clipboard;
+
+	if (!SetClipboardData(CF_UNICODETEXT, hData))
+		goto close_clipboard;
+
 	CloseClipboard();
+	return;
+
+close_clipboard:
+	CloseClipboard();
+close_hdata:
+	GlobalFree(hData);
 }
 
-static int cb_copy_address_base_relative(t_table *pt, wchar_t *name, ulong index, int mode)
+static int cb_copy(t_table *pt, wchar_t *name, ulong index, int mode)
 {
 	uint32_t address;
+	enum copy_mode copy_mode = (enum copy_mode)index;
 
 	if ((mode != MENU_EXECUTE && mode != MENU_VERIFY) || (address = get_selection(pt)) == 0)
 		return MENU_ABSENT;
 	if (mode == MENU_VERIFY)
 		return MENU_NORMAL;
 
-	copy_to_clipboard(address, BASE_RELATIVE);
+	copy_to_clipboard(address, copy_mode);
 	return MENU_NOREDRAW;
 };
 
-static int cb_copy_address_file_relative(t_table *pt, wchar_t *name, ulong index, int mode)
+static int cb_follow(t_table *pt, wchar_t *name, ulong index, int mode)
 {
 	uint32_t address;
-
-	if ((mode != MENU_EXECUTE && mode != MENU_VERIFY) || (address = get_selection(pt)) == 0)
-		return MENU_ABSENT;
-	if (mode == MENU_VERIFY)
-		return MENU_NORMAL;
-
-	copy_to_clipboard(address, FILE_RELATIVE);
-	return MENU_NOREDRAW;
-};
-
-static int cb_copy_dword_base_relative(t_table *pt, wchar_t *name, ulong index, int mode)
-{
-	uint32_t address;
+	enum follow_mode follow_mode = (enum follow_mode)index;
 
 	if ((mode != MENU_EXECUTE && mode != MENU_VERIFY) || (address = get_selection(pt)) == 0)
 		return MENU_ABSENT;
@@ -152,65 +152,19 @@ static int cb_copy_dword_base_relative(t_table *pt, wchar_t *name, ulong index, 
 		return MENU_REDRAW;
 	}
 
-	copy_to_clipboard(address, BASE_RELATIVE);
-	return MENU_NOREDRAW;
-};
-
-static int cb_copy_dword_file_relative(t_table *pt, wchar_t *name, ulong index, int mode)
-{
-	uint32_t address;
-
-	if ((mode != MENU_EXECUTE && mode != MENU_VERIFY) || (address = get_selection(pt)) == 0)
-		return MENU_ABSENT;
-	if (mode == MENU_VERIFY)
-		return MENU_NORMAL;
-
-	Readmemory(&address, address, 4, MM_REPORT);
-	if (!Findmemory(address)) {
-		Flash(L"No memory at the specified address");
-		return MENU_REDRAW;
+	switch (follow_mode) {
+	case asm:
+		Setcpu(0, address, 0, 0, 0, CPU_ASMHIST|CPU_ASMCENTER|CPU_ASMFOCUS);
+		break;
+	case dump:
+		Setcpu(0, 0, address, 0, 0, CPU_DUMPHIST|CPU_DUMPFOCUS);
+		break;
+	case stack: default:
+		Setcpu(0, 0, 0, 0, address, CPU_STACKCTR|CPU_STACKFOCUS);
+		break;
 	}
 
-	copy_to_clipboard(address, FILE_RELATIVE);
 	return MENU_NOREDRAW;
-};
-
-static int cb_follow_in_disassembler(t_table *pt, wchar_t *name, ulong index, int mode)
-{
-	uint32_t address;
-
-	if ((mode != MENU_EXECUTE && mode != MENU_VERIFY) || (address = get_selection(pt)) == 0)
-		return MENU_ABSENT;
-	if (mode == MENU_VERIFY)
-		return MENU_NORMAL;
-
-	Readmemory(&address, address, 4, MM_REPORT);
-	if (!Findmemory(address)) {
-		Flash(L"No memory at the specified address");
-		return MENU_REDRAW;
-	}
-
-	Setcpu(0, address, 0, 0, 0, CPU_ASMHIST|CPU_ASMCENTER|CPU_ASMFOCUS);
-	return MENU_NOREDRAW;
-};
-
-static int cb_follow_in_dump(t_table *pt, wchar_t *name, ulong index, int mode)
-{
-	uint32_t address;
-
-	if ((mode != MENU_EXECUTE && mode != MENU_VERIFY) || (address = get_selection(pt)) == 0)
-		return MENU_ABSENT;
-	if (mode == MENU_VERIFY)
-		return MENU_NORMAL;
-
-	Readmemory(&address, address, 4, MM_REPORT);
-	if (!Findmemory(address)) {
-		Flash(L"No memory at the specified address");
-		return MENU_REDRAW;
-	}
-
-	Setcpu(0, 0, address, 0, 0, CPU_DUMPHIST|CPU_DUMPFOCUS);
-	return MENU_REDRAW;
 };
 
 static int cb_about(t_table *pt, wchar_t *name, ulong index, int mode)
@@ -222,42 +176,45 @@ static int cb_about(t_table *pt, wchar_t *name, ulong index, int mode)
 
 	Resumeallthreads();
 	MessageBox(hwollymain,
-		L"Address shortcuts\r\n"
-		L"Authors: Andrew D'Addesio <andrew.daddesio@utexas.edu>\r\n"
+		L"Address Shortcuts\r\n"
+		L"Authors: Andrew D'Addesio\r\n"
 		L"URL: https://github.com/daddesio/ollydbg-address-shortcuts\r\n\r\n"
-		L"This plugin is public domain software.",
-		L"Address shortcuts", MB_OK | MB_ICONINFORMATION);
+		L"This plugin is public domain software released under the UNLICENSE.",
+		L"Address Shortcuts", MB_OK | MB_ICONINFORMATION);
 	Suspendallthreads();
 
 	return MENU_NOREDRAW;
 };
 
 static t_menu main_menu[] = {
-	{L"|About address shortcuts...",
-		L"About Address shortcuts",
+	{L"|About Address Shortcuts...",
+		L"About Address Shortcuts",
 		K_NONE, cb_about, NULL, {0} },
 	{NULL, NULL, K_NONE, NULL, NULL, {0} }
 };
 
 static t_menu right_click_menu[] = {
-	{ L"Copy address base relative",
-		L"Copy address of selection to clipboard, relative to module base",
-		KK_DIRECT|KK_ALT|VK_OEM_PLUS, cb_copy_address_base_relative, NULL, {0} },
-	{ L"Copy address file relative",
-		L"Copy address of selection to clipboard, relative to module file start",
-		KK_DIRECT|KK_CTRL|VK_OEM_PLUS, cb_copy_address_file_relative, NULL, {0} },
-	{ L"Copy DWORD base relative",
-		L"Copy doubleword to clipboard, relative to module base",
-		KK_DIRECT|KK_ALT|VK_OEM_MINUS, cb_copy_dword_base_relative, NULL, {0} },
-	{ L"Copy DWORD file relative",
-		L"Copy doubleword to clipboard, relative to module file start",
-		KK_DIRECT|KK_CTRL|VK_OEM_MINUS, cb_copy_dword_file_relative, NULL, {0} },
 	{ L"Follow DWORD in Disassembler",
 		L"Follow doubleword in CPU Disassembler",
-		KK_DIRECT|KK_CTRL|VK_RETURN, cb_follow_in_disassembler, NULL, {0} },
+		KK_DIRECT|KK_CTRL|VK_RETURN, cb_follow, NULL, {asm} },
 	{ L"Follow DWORD in Dump",
 		L"Follow doubleword in CPU Dump",
-		KK_DIRECT|KK_ALT|VK_RETURN, cb_follow_in_dump, NULL, {0} },
+		KK_DIRECT|VK_RETURN, cb_follow, NULL, {dump} },
+	{ L"Follow DWORD in Stack",
+		L"Follow doubleword in CPU Stack",
+		0, cb_follow, NULL, {stack} },
+	{ L"Copy RVA",
+		L"Copy RVA of selection to clipboard",
+		0, cb_copy, NULL, {rva} },
+	{ L"Copy RVA (pretty)",
+		L"Copy RVA of selection to clipboard in the format module+offset",
+		KK_DIRECT|KK_ALT|VK_OEM_PLUS, cb_copy, NULL, {rva_pretty} },
+	{ L"Copy file offset",
+		L"Copy file offset of selection to clipboard",
+		0, cb_copy, NULL, {file_offset} },
+	{ L"Copy file offset (pretty)",
+		L"Copy file offset of selection to clipboard in the format file+offset",
+		KK_DIRECT|KK_CTRL|VK_OEM_PLUS, cb_copy, NULL, {file_offset_pretty} },
 	{ NULL, NULL, K_NONE, NULL, NULL, {0} }
 };
 
@@ -267,6 +224,7 @@ extc t_menu * __cdecl ODBG2_Pluginmenu(wchar_t *type)
 		return main_menu;
 	if (!wcscmp(type, PWM_DISASM) || !wcscmp(type, PWM_DUMP) || !wcscmp(type, PWM_STACK))
 		return right_click_menu;
+
 	return NULL;
 };
 
@@ -289,7 +247,7 @@ extc int ODBG2_Pluginquery(int ollydbgversion, ulong *features,
 	if (ollydbgversion < 201)
 		return 0;
 
-	wcscpy(pluginname, L"Address shortcuts");
+	wcscpy(pluginname, L"Address Shortcuts");
 	wcscpy(pluginversion, L"0");
 	return PLUGIN_VERSION;
 };
